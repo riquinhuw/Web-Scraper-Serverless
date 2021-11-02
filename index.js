6b079c9367c6287bbb5214ee89afcd8f10468849
Carlos H <riquinhuw_lass@hotmail.com>
2021-11-02 15:39:55 -0300

    Configuração do Serverless para deploy

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const puppeteer = require('puppeteer');
+console.log('Iniciando função');
 
-//TODO: Transformar em uma função separada
+const AWS = require('aws-sdk');
+const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+let produtosBestsellers = [];
+
+
+(async()=>{
+  produtosBestsellers = await buscarBestsellers();
+  console.log(produtosBestsellers);
+})();
+
+
 //TODO: usar then,catch
-(async () => {
-    const browser = await puppeteer.launch();  
+async function buscarBestsellers() {
+    const baseUrl ='https://www.amazon.com.br';
+    const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
     const page = await browser.newPage();
     await page.setViewport({
       width: 1920,
       height: 1080
   })
-    await page.goto('https://www.amazon.com.br/');
-    await page.screenshot({path: 'pessoal/example.png'}); 
+    await page.goto(baseUrl);
+    //await page.screenshot({path: 'pessoal/example.png'}); 
     await page.goto('https://www.amazon.com.br/bestsellers');
-    await page.screenshot({path: 'pessoal/example1.png'});  
+    console.log(page.url());
+    //await page.screenshot({path: 'pessoal/example1.png'});  
 
     //TODO: Melhorar a busca para trazer cada <li> como um obj
     /**
@@ -20,12 +41,51 @@ const puppeteer = require('puppeteer');
      * Dessa forma só estou trazendo 3 primeiros itens de uma categoria só.
      */
     const nomes = await page.$$eval('#anonCarousel1 > ol > li> div > div > a > span > div ', titles =>
-    titles.map(titles => titles.textContent)
-    );  
+    titles.map(titles => titles.textContent));  
     const valores = await page.$$eval('#anonCarousel1 > ol > li > div > div > div > a > span > span ', titles =>
-    titles.map(titles => titles.textContent)
-    );
-    console.log(nomes);
-    console.log(valores);
+    titles.map(titles => titles.textContent));
+    const links = await page.$$eval('#anonCarousel1 > ol > li > div > div > a:nth-child(2)', titles =>
+    titles.map(titles => titles.getAttribute('href')));
+
     await browser.close();
-  })();
+    console.log(nomes,valores,links);
+    return nomes.reduce((acumulador,valorAtual,index)=>{
+      return acumulador =[...acumulador,{nome:valorAtual,valor:valores[index],link:baseUrl+links[index]}]
+    },[]);
+  };
+
+exports.handler = async (event, context,callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+
+    let response;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+    
+    try {
+        let registros = await dynamo.scan({ TableName: "tabelateste" }).promise();
+        
+        //Limpar os Registros
+        for (var x = 0; x < registros.Items.length; x++) {
+            response = await  dynamo.delete({Key:registros.Items[x].particaoteste}).promise()
+        }
+        
+        //Adicionar os novos registros
+        for (var i = 0; i < produtosBestsellers.length; i++) {
+            response = await dynamo.put({...produtosBestsellers[i],particaoteste:i}).promise();
+        }
+        
+    } catch (err) {
+        statusCode = '400';
+        response = err.message;
+    } finally {
+        response = JSON.stringify(response);
+    }
+
+    return {
+        statusCode,
+        response,
+        headers,
+    };
+};
